5d884276e720798b620e99083f49a987ec152c96
Urmas Rist <urmas@urist.ee>
2023-01-02 05:17:11 +0200

    initial commit
    Read filename and output new filename

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+class Asm
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            WriteLine("USAGE: asm <FILE PATH>");
+            Environment.Exit(1);
+        }
+        string filename = args[0];
+        if (!File.Exists(filename))
+        {
+            WriteLine("{0} not found", filename);
+            // Exit
+        }
+        string outputName = OutputName(filename);
+        WriteLine("Final output name: {0}", outputName);
+    }
+
+    private static string OutputName(string filename)
+    {
+        string outputName;
+
+        string[] extensions = filename.Split('.');
+        if (extensions.Length < 2)
+        {
+            // No extension, use whole filename for output
+            outputName = filename;
+        }
+        else
+        {
+            outputName = String.Join('.', extensions.SkipLast(1));
+        }
+        outputName = outputName + ".hack";
+        // Deal with forward slashes
+        return Path.GetFileName(outputName);
+    }
+}
